5f0d73c2077075bef33cbc06d4d9435a9f006a68
Alan Wickersham <awickersham@blizzard.com>
2019-06-26 17:50:02 -0700

    updates

--- color_defs.ps1
+++ color_defs.ps1
@@ -57,14 +57,26 @@ function Get-ColorSwatches () {
         [ValidateRange( 1,9 )]
         [decimal] $Size = 3,
 
+        [Parameter(ParameterSetName = 'Family' )]
         [string]$Family,
 
-        [string]$Level
+        [Parameter(ParameterSetName = 'Family' )]
+        [string]$Level,
+
+        [Parameter(ParameterSetName = 'Names' )]
+        [string[]]$Set
     )
 
-    $xterm.Keys | ForEach-Object {
-        if ($Family -eq $xterm.$_.$Level) {
-            $ColorNameArray += @($_)
+    switch ($PSCmdlet.ParameterSetName) {
+        "Family" {
+            $xterm.Keys | ForEach-Object {
+                if ($Family -eq $xterm.$_.$Level) {
+                    $ColorNameArray += @($_)
+                }
+            }
+        }
+        "Names"  {
+            $ColorNameArray = $Set
         }
     }
 
@@ -90,14 +102,26 @@ function Get-ColorSwatches () {
 
 function Get-ColorCards {
     param(
+        [Parameter(ParameterSetName = 'Family' )]
         [string]$Family,
 
-        [string]$Level
+        [Parameter(ParameterSetName = 'Family' )]
+        [string]$Level,
+
+        [Parameter(ParameterSetName = 'Names' )]
+        [string[]]$Set
     )
 
-    $xterm.Keys | ForEach-Object {
-        if ($Family -eq $xterm.$_.$Level) {
-            $ColorNameArray += @($_)
+    switch ($PSCmdlet.ParameterSetName) {
+        "Family" {
+            $xterm.Keys | ForEach-Object {
+                if ($Family -eq $xterm.$_.$Level) {
+                    $ColorNameArray += @($_)
+                }
+            }
+        }
+        "Names"  {
+            $ColorNameArray = $Set
         }
     }
 
@@ -107,20 +131,21 @@ function Get-ColorCards {
         $colorBit = $xterm.$_.tbit
 
         $line1 += "$_$BG$colorBit$(" " * (20 - "$_".Length) )$Clir"
-        $line2 += "$BG$colorBit$(" " * 20)$Clir"
+        $line2 += "$($xterm.$_.hexString)$BG$colorBit$(" " * 13)$Clir"
+        $line3 += "$BG$colorBit$(" " * 20)$Clir"
 
         if ( ( ( $Counter % 6 ) -eq 0 ) -or ( $Counter -eq $ColorNameArray.Count ) ) {
             Write-Host $line1
-            foreach ( $i in 2..6 ) {
-                Write-Host $line2
+            Write-Host $line2
+            foreach ( $i in 3..6 ) {
+                Write-Host $line3
             }
         }
 
-        if ( ( $Counter % 6 ) -eq 0 ) { $line1 = ""; $line2 = "" }
+        if ( ( $Counter % 6 ) -eq 0 ) { $line1 = ""; $line2 = ""; $line3 = "" }
     }
 
-    $line1 = ""; $line2 = ""; $Counter = 0
-    
+    $line1 = ""; $line2 = ""; $line3 = ""; $Counter = 0
 }
 
 # Functions for setting color themes
@@ -160,5 +185,99 @@ function Set-ColorTheme () {
     $PSColorTheme | ForEach-Object {
         $Themelet = $_
         Set-ColorThemeValues @Themelet
+    }
+}
+
+function Convert-RGBToHSL
+{
+    [CmdLetBinding()]
+    param
+    (
+        [Parameter(Mandatory=$true, Position=0, ValueFromPipelineByPropertyName=$true)][ValidateRange(0, 0xFF)][int]$Red,
+        [Parameter(Mandatory=$true, Position=1, ValueFromPipelineByPropertyName=$true)][ValidateRange(0, 0xFF)][int]$Green,
+        [Parameter(Mandatory=$true, Position=2, ValueFromPipelineByPropertyName=$true)][ValidateRange(0, 0xFF)][int]$Blue
+    )
+    process
+    {
+        $arry = @(($red / 255), ($green / 255), ($blue / 255))
+        $max = 0
+        $min = 0
+        for($i = 1; $i -lt $arry.Length; $i++)
+        {
+            if($arry[$i] -gt $arry[$max])
+            {
+                $max = $i
+            }
+
+            if($arry[$i] -lt $arry[$min])
+            {
+                $min = $i
+            }
+        }
+        $C = $arry[$max] - $arry[$min]
+        $hue = 0
+        $lightness = 0
+        if(0 -ne $C)
+        {
+            $hue = ($arry[($max + 4) % 3] - $arry[($max+5) % 3]) / $C
+
+            if(0 -eq $max)
+            {
+                $hue %= 6
+            }
+            else
+            {
+                $hue += 2*$max
+            }
+
+            $hue *= 60
+            if ($hue -lt 0) {$hue = $hue + 360}
+            $lightness = 0.5 * ($arry[$min] + $arry[$max])
+            $saturation = $C / (1 - [Math]::Abs(2.0 * $lightness - 1)) * 100
+            $lightness *= 100
+        }
+        return [pscustomobject]@{hue = $( "{0:N2}" -f $hue); saturation = $( "{0:N2}" -f $saturation); lightness = $( "{0:N2}" -f $lightness)} | Format-List
+    }
+}
+
+function Get-ClosestXTermColor () {
+    $HexString = $args
+
+    $r = "0x$($HexString.Substring(1,2))"
+    $g = "0x$($HexString.Substring(3,2))"
+    $b = "0x$($HexString.Substring(5,2))"
+
+    $rx = [int32]$r
+    $gx = [int32]$g
+    $bx = [int32]$b
+
+    $rx = $rx - 15
+    $rx = $rx / 40
+    $rx = [math]::Round($rx)
+    $rx *= 40
+    $rx += 15
+    if ($rx -eq 15) {$rx = 0}
+    if ($rx -eq 55) {$rx = 95}
+    
+    $gx = $gx - 15
+    $gx = $gx / 40
+    $gx = [math]::Round($gx)
+    $gx *= 40
+    $gx += 15
+    if ($gx -eq 15) {$gx = 0}
+    if ($gx -eq 55) {$gx = 95}
+
+    $bx = $bx - 15
+    $bx = $bx / 40
+    $bx = [math]::Round($bx)
+    $bx *= 40
+    $bx += 15
+    if ($bx -eq 15) {$bx = 0}
+    if ($bx -eq 55) {$bx = 95}
+
+    $rgbString = "rgb($rx,$gx,$bx)"
+
+    $xterm.Keys | ForEach-Object {
+        if ($xterm.$_.rgbString -eq $rgbString) {Write-Host "$_`:".PadRight(24," ")$xterm.$_.hexString}
     }
 }
---
+++ w3rusticCarbon.ps1
@@ -0,0 +1,50 @@
+$PSColorTheme = @(
+    @{  #History ID
+        Back = 1
+        NewColor = "Caramel"
+    },
+    @{  #PS Version
+        Back = 2
+        NewColor = "Grey27"
+    },
+    @{  #Directory
+        Back = 3
+        NewColor = "YellowStone"
+    },
+    @{  #Command Duration
+        Back = 4
+        NewColor = "Grey27"
+    },
+    @{  #Date
+        Back = 5
+        NewColor = "YellowStone"
+    },
+    @{  #Time
+        Back = 6
+        NewColor = "Maroon"
+    },
+    @{  #History ID
+        Text = 1
+        NewColor = "Canvas"
+    },
+    @{  #PS Version
+        Text = 2
+        NewColor = "Canvas"
+    },
+    @{  #Directory
+        Text = 3
+        NewColor = "Charcoal"
+    },
+    @{  #Command Duration
+        Text = 4
+        NewColor = "Canvas"
+    },
+    @{  #Date
+        Text = 5
+        NewColor = "Charcoal"
+    },
+    @{  #Time
+        Text = 6
+        NewColor = "Canvas"
+    }
+) # w3schools 2016 Color Palette
